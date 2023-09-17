26ce27199a789b7ddd5604c62d67d5cb12168a3c
neokkk <seven3126@gmail.com>
2023-09-17 20:37:25 +0900

    chore: 코드 정리

--- packages/server/src/entities/node/node.controller.ts
+++ packages/server/src/entities/node/node.controller.ts
@@ -1,51 +1,13 @@
 import { Controller, Get, Logger, Render } from '@nestjs/common';
-import { MqttClient, connect } from 'mqtt';
 import { NodeGateway } from './node.gateway';
-import { NodeService } from './node.service';
-import { SensorData, SystemData } from './node.type';
 
 @Controller()
 export class NodeController {
   private logger = new Logger('NodeController');
-  private client: MqttClient;
 
   constructor(
-    private readonly nodeService: NodeService,
     private readonly nodeGateway: NodeGateway,
-  ) {
-    this.client = connect('mqtt://192.168.31.128:1883');
-
-    this.client.on('connect', () => {
-      this.logger.log('on mqtt connect');
-      this.client.subscribe('sensor', (err) => {
-        if (err) {
-          this.logger.error('on subscribe sensor error');
-        }
-      });
-      this.client.subscribe('system', (err) => {
-        if (err) {
-          this.logger.error('on subscribe system error');
-        }
-      })
-    });
-
-    this.client.on('message', (topic, buf) => {
-      this.logger.log('on mqtt message');
-
-      switch (topic) {
-        case 'sensor':
-          const [temp, press] = [buf.readFloatLE(0), buf.readFloatLE(4)];
-          const sensorData = { __type: topic, temp, press };
-          this.nodeGateway.sendData(sensorData);
-          break;
-        case 'system':
-          const systemData = JSON.parse(buf.toString());
-          systemData.__type = topic;
-          this.nodeGateway.sendData(systemData);
-          break;
-      }
-    });
-  }
+  ) {}
 
   @Get()
   @Render('node/index')
--- packages/server/src/entities/node/node.gateway.ts
+++ packages/server/src/entities/node/node.gateway.ts
@@ -1,39 +1,95 @@
 import { Injectable, Logger } from '@nestjs/common';
+import { MqttClient, connect } from 'mqtt';
 import { WebSocket, WebSocketServer } from 'ws';
 
 @Injectable()
 export class NodeGateway {
   private logger = new Logger('NodeGateway');
-  private server: WebSocketServer;
+  private wss: WebSocketServer;
+  private mqc: MqttClient;
 
   constructor() {
-    this.server = new WebSocketServer({ port: 81 });
-    this.setEvents();
+    this.mqc = connect('mqtt://0.0.0.0:1883');
+    this.wss = new WebSocketServer({ port: 81 });
+
+    this.setMqttEvents();
+    this.setWebsocketEvents();
   }
 
-  private setEvents() {
-    this.server.on('connection', (socket) => {
+  private setMqttEvents() {
+    this.mqc.on('connect', () => {
+      this.logger.log('on mqtt connect');
+      this.mqc.subscribe('sensor', (err) => {
+        if (err) {
+          this.logger.error('on subscribe sensor error');
+        }
+      });
+      this.mqc.subscribe('system', (err) => {
+        if (err) {
+          this.logger.error('on subscribe system error');
+        }
+      })
+    });
+
+    this.mqc.on('message', (topic, buf) => {
+      this.logger.log('on mqtt message');
+
+      switch (topic) {
+        case 'sensor':
+          const [temp, press] = [buf.readFloatLE(0), buf.readFloatLE(4)];
+          const sensorData = { __type: topic, temp, press };
+          this.sendWsData(sensorData);
+          break;
+        case 'system':
+          const systemData = JSON.parse(buf.toString());
+          systemData.__type = topic;
+          this.sendWsData(systemData);
+          break;
+      }
+    });
+  }
+
+  private setWebsocketEvents() {
+    this.wss.on('connection', (socket) => {
       this.logger.log('on connection');
+
       socket.on('message', (message: string) => {
-        this.logger.log('on messag', message);
+        this.logger.log('on message', message);
+        const messageObj = JSON.parse(message);
+        if (messageObj.__type === 'engine') {
+          this.sendMqttData({
+            num: messageObj.num,
+            speed: messageObj.speed,
+          });
+        }
       });
       socket.on('disconnect', () => {
         this.logger.log('on disconnect');
       });
     });
 
-    this.server.on('error', (error) => {
+    this.wss.on('error', (error) => {
       this.logger.error('on error', error);
-      this.server.close();
+      this.wss.close();
     });
   }
 
-  sendData(data: any) {
-    if (!this.server) return;
+  sendWsData(data: any) {
+    if (!this.wss) return;
 
-    this.server.clients.forEach((client) => {
+    this.wss.clients.forEach((client) => {
       if (client.readyState === WebSocket.OPEN) {
         client.send(JSON.stringify(data));
+      }
+    });
+  }
+
+  sendMqttData(data: any) {
+    if (!this.mqc) return;
+
+    this.mqc.publish('engine', JSON.stringify(data), (err) => {
+      if (err) {
+        this.logger.error('on publish engine error');
       }
     });
   }
